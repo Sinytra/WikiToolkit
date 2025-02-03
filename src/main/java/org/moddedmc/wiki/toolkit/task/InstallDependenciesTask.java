d9fc42d6398250dfc2525d2f957b1980af102fcf
Cassian Godsted <cassian@godsted.com>
2025-02-02 19:21:06 -0500

    Fix Linux preview with IntelliJ

--- src/main/java/org/moddedmc/wiki/toolkit/task/InstallDependenciesTask.java
+++ src/main/java/org/moddedmc/wiki/toolkit/task/InstallDependenciesTask.java
@@ -1,10 +1,17 @@
 package org.moddedmc.wiki.toolkit.task;
 
+import org.gradle.internal.os.OperatingSystem;
 import org.gradle.work.DisableCachingByDefault;
 
 @DisableCachingByDefault
 public abstract class InstallDependenciesTask extends ExecuteCommandTask {
     public InstallDependenciesTask() {
-        getCommand().addAll("npm", "install");
+        if (OperatingSystem.current().isLinux()) {
+            getCommand().addAll("bash", "-ci", "npm install");
+        } else if (OperatingSystem.current().isMacOsX()) {
+            getCommand().addAll("bash", "-c", "npm install");
+        } else {
+            getCommand().addAll("npm", "install");
+        }
     }
 }
